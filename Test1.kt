2265576335f74fabe1fb10f8169bba55fbd3ef8b
오!지은입니다 <w2317@e-mirim.hs.kr>
2024-04-02 19:54:15 +0900

    문제

--- Test1.kt
+++ Test1.kt
@@ -1,12 +1,24 @@
-// val, var
-// 2
-// 2
-// 2
-// X
-// str?.length?:0
+package chapter2
 
-fun main() {
-    var str:String? = "Hello"
-    var result = str?.length?:0
+fun main(){
+    print("점수를 입력해주세요 > ")
+    val score = readLine()!!.toInt()
+    val result = if(score in 1..100){
+        if(score >= 90) {
+            "A"
+        } else if(score >= 80) {
+            "B"
+        } else if(score >= 70) {
+            "C"
+        } else if(score >= 60) {
+            "D"
+        } else {
+            "F"
+        }
+    }
+    else {
+        "점수를 잘못 입력하셨습니다"
+    }
+
     println(result)
 }
